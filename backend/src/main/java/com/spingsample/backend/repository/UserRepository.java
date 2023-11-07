3efecd6fa29c7d2d24f0a6fd68399ac25ea166e6
Chamod Dilpa Gunarathna <chamodgunarathna06@gmail.com>
2023-11-08 00:58:18 +0530

    Front End Appllication

--- backend/src/main/java/com/spingsample/backend/repository/UserRepository.java
+++ backend/src/main/java/com/spingsample/backend/repository/UserRepository.java
@@ -1,4 +1,7 @@
 package com.spingsample.backend.repository;
 
-public interface UserRepository {
+import com.spingsample.backend.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
